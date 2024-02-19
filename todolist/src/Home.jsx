e099c8215428b8a8b29f62ae3594943433049544
Poorvi Singh Thakur <poorvisinghthakur27@gmail.com>
2024-02-19 19:14:22 +0530

    firstCommit

---
+++ todolist/src/Home.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import Create from './Create'
+
+
+
+const Home = () => {
+    const [todos,setTodos] = useState([]);
+  return (
+    <div>
+      <h2>To do List</h2>
+      <Create />
+      {
+        
+        todos.length===0?
+        <div>No Record</div>
+        :
+        todos.map(todo => (
+        <div>
+            {todo}
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Home
